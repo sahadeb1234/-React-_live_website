999890040f54dce8ff46248bb5e30716b1fc8a93
Sahadeb <np03a190065@heraldcollege.edu.np>
2021-10-30 15:03:17 +0545

    Updates

---
+++ precache-manifest.8fb7bd8c9c1b47c0791b70efedcd0ea5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909937792af368e8db25cd6e632433c8",
+    "url": "/-React-_live_website/index.html"
+  },
+  {
+    "revision": "20177ff0009363bf2949",
+    "url": "/-React-_live_website/static/css/main.4f727191.chunk.css"
+  },
+  {
+    "revision": "4d4ab29db50c5c837597",
+    "url": "/-React-_live_website/static/js/2.412263c6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/-React-_live_website/static/js/2.412263c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20177ff0009363bf2949",
+    "url": "/-React-_live_website/static/js/main.921d559a.chunk.js"
+  },
+  {
+    "revision": "f24bc9cce9faacf23211",
+    "url": "/-React-_live_website/static/js/runtime-main.c3b70299.js"
+  },
+  {
+    "revision": "542955c7018a193598f389bc3d7c1c11",
+    "url": "/-React-_live_website/static/media/Sahadeb_Rokaya_cv.542955c7.pdf"
+  },
+  {
+    "revision": "f5711eaa4996e1483f221e7c57cb7905",
+    "url": "/-React-_live_website/static/media/algo.f5711eaa.svg"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/-React-_live_website/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "a42040d5c68d4d3fed9b27878d2fcca5",
+    "url": "/-React-_live_website/static/media/backend.a42040d5.svg"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/-React-_live_website/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "0a27bbff6aa4dfb3bf8606c2fba69fd6",
+    "url": "/-React-_live_website/static/media/facebook.0a27bbff.svg"
+  },
+  {
+    "revision": "cbc38ef18c5a6ecdc0ace14b23e84380",
+    "url": "/-React-_live_website/static/media/github.cbc38ef1.svg"
+  },
+  {
+    "revision": "d08bce0dcb53ad1ef20b0cae18977145",
+    "url": "/-React-_live_website/static/media/instagram.d08bce0d.svg"
+  },
+  {
+    "revision": "a13311a36cde2cfa79bff078a24103bf",
+    "url": "/-React-_live_website/static/media/mightycoder.a13311a3.svg"
+  },
+  {
+    "revision": "64c56c72135ba0b4ddf8da186182b723",
+    "url": "/-React-_live_website/static/media/pin.64c56c72.svg"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/-React-_live_website/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/-React-_live_website/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/-React-_live_website/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/-React-_live_website/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/-React-_live_website/static/media/tie.4a04f9f0.svg"
+  }
+]);
